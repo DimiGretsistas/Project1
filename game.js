12c89e9085f92afa6a9f8584fdcf0e513252d5fc
Dimitrios Gretsistas <dimitriosgretsistas@gmail.com>
2023-08-21 15:12:54 +0200

    first files

---
+++ game.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+const gameContainer = document.getElementById('game-container');
+
+class Game {
+    constructor() {
+        this.startScreen = document.getlElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.endScreen = document.getElementById(game-end);
+        this.player = new this.player(
+            this.gameScreen,
+            200,
+            500,
+            100,
+            150, // they all need to be changed
+            "../images/diver.gif"
+        );
+    }
+}
+
+
+
+
+});
